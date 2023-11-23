c41a99689de7d4e110066ce846ee7f5476eb2bda
daniel.illanes <daniel.illanes@orenesgrupo.com>
2023-11-23 12:00:30 +0100

    Project base

---
+++ TurboMath.cpp
@@ -0,0 +1,14 @@
+#include "TurboMath.h"
+
+TurboMath::TurboMath()
+{
+}
+
+TurboMath::~TurboMath()
+{
+}
+
+int TurboMath::restar(const int& x, const int& y)
+{
+	return x-y;
+}
---
+++ TurboMath.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "math-lib-ex/CustomMath.h"
+
+class TurboMath : math_lib_ex::CustomMath
+{
+	TurboMath();
+	~TurboMath();
+	int restar(const int& x, const int& y);
+};
+
